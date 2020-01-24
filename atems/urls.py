522b484dadb00c2d090476abe1895f29173dbd4c
luizfernando <luiz-fsilva37@hotmail.com>
2020-01-24 13:25:41 -0300

    Atualização urls projeto

--- atems/urls.py
+++ atems/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('atems/distribuidoras/', views.distribuidoras, name='distribuidoras'),
-    path('atems/distribuidora/jogos', views.jogos, name='jogos')
+    path('atems/distribuidoras/', views.listar_distribuidoras, name='listar_distribuidoras'),
+    path('atems/adicionar/distribuidora', views.adicionar_distribuidora, name='adicionar_distribuidora'),
+    path('atems/distribuidora/jogos', views.listar_jogos, name='listar_jogos'),
 ]
